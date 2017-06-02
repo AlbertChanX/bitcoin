0955e79e6ac26b8614b36334c3754a3b9f660a07
cyc <292368101@qq.com>
2017-06-02 10:12:04 +0800

    print()

--- test.py
+++ test.py
@@ -33,18 +33,24 @@ def get_blance(us):
 def get_back(balance, inputs, outputs, fee):
     txin_num = len(inputs)
     txout_num = len(outputs) + 1
+
     ###计算fee    10 + in * 148 + out * 34
     tx_bytes = (4 + 4 + 1 + 1 + txin_num * (106 + 32 + 4 + 4 + 1 + 1) + txout_num * (25 + 8 + 1))
-    print tx_bytes
+    print(tx_bytes)
     all_fee = tx_bytes / 1024.0 * fee
+
+    ###计算fee   148   34
+    tx_bytes = (4 + 4 + 1 + 1 + txin_num * (106 + 32 + 4 + 4 + 1 + 1) + txout_num * (25 + 8 + 1))
+    all_fee = tx_bytes / 1000.0 * fee
+
     back = balance - amount - numto8(all_fee)
-    print numto8(all_fee)
+    print(numto8(all_fee))
     return back
 
 
 def send_btc(address, amount, fee):
-    us = rpc_connection.listunspent()   #6-9999999,filter_address)
-    if len(us)==0:
+    us = rpc_connection.listunspent()   #6-9999999,filter_address
+    if len(us) == 0:
         logging.error('unspent is %s'%len(us))
         return None
     balance = get_blance(us)
@@ -60,25 +66,30 @@ def send_btc(address, amount, fee):
     # 设置找零
     outputs.setdefault(us[0]['address'], back)
     logging.critical('outputs is %s'%outputs)
-    ### inputs : json array, outputs:json object     outputs ???
+    # inputs : json array, outputs:json object     outputs ???
     hex = rpc_connection.createrawtransaction(inputs, outputs)
     logging.critical('create_hex: %s'%hex)   ###返回十六进制的字符串
+    pretxout = []
+    try:
+        pretxout = rpc_connection.listtransactions()
+    except Exception as e:
+        pass
 
-    ### sign
+    # sign
     sign = rpc_connection.signrawtransaction(hex)
     logging.critical('signed hex: %s'%sign['hex'])
 
-    ### send
-    tx_hash = rpc_connection.sendrawtransaction(sign['hex'])  #sign['hex'] 带上签名的hex
-    return  tx_hash
+    # send
+    tx_hash = rpc_connection.sendrawtransaction(sign['hex'])  # sign['hex'] 带上签名的hex
+    return tx_hash
 
 
-### gettransaction  get from in-wallet
+# gettransaction  get from in-wallet
 # tx_info = rpc_connection.getrawtransaction("1e22252acf6daa88f17bd0d3a83b16d98e777f50b4f7959e9ad20689277b3f59",True)
 # print tx_info
-###decoderawtransaction   -->  inputs,outputs
-#decode = rpc_connection.decoderawtransaction(hex)
-#print decode
+# decoderawtransaction   -->  inputs,outputs
+# decode = rpc_connection.decoderawtransaction(hex)
+# print decode
 
 
 
@@ -90,8 +101,6 @@ if __name__ == '__main__':
     fee = 0.00001  # BTC/kB
 
     send_btc(address, amount, fee)
-
-
 
 
 
