9eee61954c7ba5e0cce83b2a242b33ab0e67c38d
cyc <292368101@qq.com>
2017-06-09 17:29:07 +0800

    add fee

--- 1hash-txinfo.py
+++ 1hash-txinfo.py
@@ -2,6 +2,7 @@
 import requests
 import time
 from openpyxl import Workbook
+import pandas as pd
 
 
 # url = 'https://chain.api.btc.com/v3/address/1F1xcRt8H8Wa623KqmkEontwAAVqDSAWCV/tx?page='
@@ -12,27 +13,36 @@ def ts2time(ts):
 tx_list = []
 for i in range(1000):
     url = 'https://chain.api.btc.com/v3/address/1F1xcRt8H8Wa623KqmkEontwAAVqDSAWCV/tx?page=%s' % (i+1)
+    print url
     flag = 0
     # session = requests.Session()
     # session.trust_env = False
     js = requests.get(url).json()
-    time.sleep(2)
+    time.sleep(2.5)
 
     for tx in js['data']['list']:
         tmp = []
         ts = ts2time(tx['block_time'])
-        txid = tx['hash']
+        print ts
+        # txid = tx['hash']
         balance = tx['balance_diff']/100000000.0
-        print ts, txid, balance
-        if ts <'2017/04/11':
+        # print ts, txid, balance
+        is_cb = tx['is_coinbase']
+        print is_cb
+        if ts >= '2017/03/01' and ts < '2017/06/01' and is_cb:
+            print(ts, is_cb)
+            tmp.append(ts)
+            tmp.append(is_cb)
+            tmp.append(balance)
+            tmp.append(balance-12.5)
+            tx_list.append(tmp)
+        elif ts < '2017/03/01':
             flag = 1
             break
-        tmp.append(ts)
-        tmp.append(txid)
-        tmp.append(balance)
-        tx_list.append(tmp)
+        # tmp.append(ts)
+        # tmp.append(txid)
+        # tmp.append(balance)
         # print(tmp)
-
     if flag == 1:
        break
 
@@ -43,6 +53,15 @@ for tx in tx_list:
     ws.append(tx)
     print(tx)
 wb.save(filename="1hash_tx.xlsx")
+
+df = pd.read_excel("1hash_tx.xlsx")
+df.columns = ['time', 'balance', 'fee']
+
+
+
+
+
+
 
 
 
